6116f1b33d724892311048abf3852a853aeb2198
abdelrahmanahmedd <132174792+abdelrahmanahmedd@users.noreply.github.com>
2023-04-30 02:00:31 +0200

    Add files via upload

---
+++ assign_02_problem_02_code.java
@@ -0,0 +1,17 @@
+/* 
+ Name: "Abdelrahman Ahmed"
+ ID:"20216681"
+*/
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i = 1; i <= 12; i++) {
+            int result = n * i;
+            System.out.println(n + " * " + i + " = " + result);
+        }
+        input.close();
+    }
+}
